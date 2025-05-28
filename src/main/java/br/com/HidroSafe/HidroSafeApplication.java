396903ed9d69989575a74d48c1aeb06d0a758f1b
Guilherme <guilhermegpereira26@hotmail.com>
2025-05-28 18:55:29 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/HidroSafe/HidroSafeApplication.java
@@ -0,0 +1,13 @@
+package br.com.HidroSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidroSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HidroSafeApplication.class, args);
+	}
+
+}
